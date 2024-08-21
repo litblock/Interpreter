7e30fd495ffc49587951da8992cc4c13b93b3e5b
litblock <37122962+litblock@users.noreply.github.com>
2024-08-20 22:58:00 -0400

    Add files via upload

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -8,6 +8,15 @@ import (
 const (
 	LEFT_PAREN  rune = '('
 	RIGHT_PAREN rune = ')'
+	LEFT_BRACE  rune = '{'
+	RIGHT_BRACE rune = '}'
+	COMMA       rune = ','
+	DOT         rune = '.'
+	MINUS       rune = '-'
+	PLUS        rune = '+'
+	SEMICOLON   rune = ';'
+	STAR        rune = '*'
+	SLASH       rune = '/'
 )
 
 func main() {
@@ -41,7 +50,26 @@ func main() {
 			fmt.Println("LEFT_PAREN ( null")
 		case RIGHT_PAREN:
 			fmt.Println("RIGHT_PAREN ) null")
+		case LEFT_BRACE:
+			fmt.Println("LEFT_BRACE { null")
+		case RIGHT_BRACE:
+			fmt.Println("RIGHT_BRACE } null")
+		case COMMA:
+			fmt.Println("COMMA , null")
+		case DOT:
+			fmt.Println("DOT . null")
+		case MINUS:
+			fmt.Println("MINUS - null")
+		case PLUS:
+			fmt.Println("PLUS + null")
+		case SEMICOLON:
+			fmt.Println("SEMICOLON ; null")
+		case STAR:
+			fmt.Println("STAR * null")
+		case SLASH:
+			fmt.Println("SLASH / null")
 		}
 	}
+	//eof = end of file
 	fmt.Println("EOF  null")
 }
