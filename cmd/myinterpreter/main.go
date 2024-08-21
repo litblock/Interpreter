b6516ebe606017f929e84c9ee60f4827f7363796
litblock <37122962+litblock@users.noreply.github.com>
2024-08-20 21:58:20 -0400

    Add files via upload

---
+++ cmd/myinterpreter/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	LEFT_PAREN  rune = '('
+	RIGHT_PAREN rune = ')'
+)
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	if command != "tokenize" {
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+
+	filename := os.Args[2]
+	raw, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fileContents := string(raw)
+
+	for _, token := range fileContents {
+		switch token {
+		case LEFT_PAREN:
+			fmt.Println("LEFT_PAREN ( null")
+		case RIGHT_PAREN:
+			fmt.Println("RIGHT_PAREN ) null")
+		}
+	}
+	fmt.Println("EOF  null")
+}
